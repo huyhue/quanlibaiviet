c2b125968096dc112db433639a1f9457ff5a1adc
huyhue <tpgiahuy5@gmail.com>
2020-08-31 11:40:04 +0700

    Updates

---
+++ precache-manifest.99e994ea4ca4822b14986152b8ad272b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "162356fdb7483355cd34b9f74c471b7a",
+    "url": "/huyhue/quanlibaiviet/index.html"
+  },
+  {
+    "revision": "44e5a4b1f84b83e9ff39",
+    "url": "/huyhue/quanlibaiviet/static/css/2.c6bca3f3.chunk.css"
+  },
+  {
+    "revision": "44e5a4b1f84b83e9ff39",
+    "url": "/huyhue/quanlibaiviet/static/js/2.4ca58147.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/huyhue/quanlibaiviet/static/js/2.4ca58147.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f6ea5265fc2f8b2fe8f",
+    "url": "/huyhue/quanlibaiviet/static/js/main.fd957f1f.chunk.js"
+  },
+  {
+    "revision": "11fc0b0b775812b3fcea",
+    "url": "/huyhue/quanlibaiviet/static/js/runtime-main.b8202443.js"
+  }
+]);
